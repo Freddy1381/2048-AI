fe18d9fd00d8dd7c1f6c69ebe21f658bee116d0d
Freddy1381 <zxu725@uw.edu>
2023-08-05 22:40:41 -0700

    weights tweaking

--- js/agent.js
+++ js/agent.js
@@ -24,19 +24,16 @@ class Agent {
         } else {
             let chanceEval = 0;
             const emptyCells = brain.grid.availableCells();
-            let values = [2, 4];
 
             if (emptyCells.length === 0) {
                 return this.evaluateGridMatrix(brain);
             }
     
             for (const cell of emptyCells) {
-                for (const value of values) {
-                    var clone = new AgentBrain(brain);
-                    clone.addTileToPos(cell.x, cell.y, value); // Assume a new tile spawns with value 2
-                    const score = this.expectiMax(clone, depth - 1, true);
-                    chanceEval += score;
-                }
+                var clone = new AgentBrain(brain);
+                clone.addTileToPos(cell.x, cell.y, 2); // Assume a new tile spawns with value 2
+                const score = this.expectiMax(clone, depth - 1, true);
+                chanceEval += score;
             }
             return chanceEval / emptyCells.length;
         }
@@ -56,7 +53,7 @@ class Agent {
         for (let i = 0; i < 4; i++) {
             brain.reset();
             if (brain.move(i)) {
-                const score = this.expectiMax(brain, 6, false);
+                const score = this.expectiMax(brain, 4, false);
                 if (score > bestScore) {
                     bestScore = score;
                     bestMove = i;
@@ -82,80 +79,6 @@ class Agent {
             }
         });
 
-        return score;
-    }
-    
-    evaluateGridWeights(board) {
-        function getLog2Value(tile) {
-            return tile === null ? 0 : Math.log2(tile.value);
-        }
-
-        const smoothnessWeight = 0.2;
-        const monotonicityWeight = 2.0;
-        const emptyCellsWeight = 1.0;
-        const maximumWeight = 5.0;
-      
-        // Calculate smoothness score
-        let smoothness = 0;
-
-        board.grid.eachCell(function (x, y, cell) {
-            if (cell) {
-                let value = Math.log2(cell.value);
-
-                let neighbors = [];
-                if (x > 0) neighbors.push(getLog2Value(board.grid.cells[x - 1][y]));
-                if (x < 3) neighbors.push(getLog2Value(board.grid.cells[x + 1][y]));
-                if (y > 0) neighbors.push(getLog2Value(board.grid.cells[x][y - 1]));
-                if (y < 3) neighbors.push(getLog2Value(board.grid.cells[x][y + 1]));
-                smoothness -= neighbors.reduce((acc, val) => acc + Math.abs(value - val), 0);
-            }
-        });
-      
-        // Calculate monotonicity score
-        let monotonicity = 0;
-        for (let i = 0; i < 4; i++) {
-          let row = [getLog2Value(board.grid.cells[i][0]), getLog2Value(board.grid.cells[i][1]), getLog2Value(board.grid.cells[i][2]), getLog2Value(board.grid.cells[i][3])];
-          for (let j = 0; j < 3; j++) {
-            if (row[j] > row[j + 1]) {
-              monotonicity += row[j] - row[j + 1];
-            } else {
-              monotonicity -= row[j + 1] - row[j];
-            }
-          }
-        }
-        for (let j = 0; j < 4; j++) {
-          let col = [getLog2Value(board.grid.cells[0][j]), getLog2Value(board.grid.cells[1][j]), getLog2Value(board.grid.cells[2][j]), getLog2Value(board.grid.cells[3][j])];
-          for (let i = 0; i < 3; i++) {
-            if (col[i] > col[i + 1]) {
-              monotonicity += col[i] - col[i + 1];
-            } else {
-              monotonicity -= col[i + 1] - col[i];
-            }
-          }
-        }
-      
-        // Calculate empty cells score
-        const emptyCells = board.grid.availableCells();
-        const emptyCellsScore = emptyCells.length > 0 ? Math.log2(emptyCells.length) : 0;
-      
-        // Calculate max value score
-        let maximumScore = 0;
-        board.grid.eachCell(function (x, y, cell) {
-            if (cell) {
-                var value = cell.value;
-                if (value > maximumScore) {
-                    maximumScore = value;
-                }
-            }
-        });
-
-        // Combine the scores with respective weights
-        const score =
-          smoothnessWeight * smoothness +
-          monotonicityWeight * monotonicity +
-          emptyCellsWeight * emptyCellsScore +
-          maximumWeight * Math.log2(maximumScore);
-      
         return score;
     };
 }
